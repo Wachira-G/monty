4afac7b996f0640d30292b3465473b49f7ae37d4
Gabriel Wachira <gabbyk.com@gmail.com>
2023-06-24 15:29:53 +0300

    Fix task 0: line count, stderr no of char printed

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	FILE *file;
 	char *lineptr = NULL, *opcode = NULL;
-	int line_number = 0;
+	int line_number = 1;
 	size_t len;
 	ssize_t nread;
 	stack_t *stack = NULL;
@@ -21,21 +21,20 @@ int main(int argc, char *argv[])
 	/* test for correct input file */
 	if (argc != 2)
 	{
-		write(2, "USAGE: monty file\n", 19);
+		write(2, "USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r"); /* open the file */
 	if (file == NULL)
 	{
-		fprintf(stderr, "Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
 	/*read line by line  */
 	while ((nread = getline(&lineptr, &len, file)) != -1)
 	{
-		line_number++;
 		opcode = strtok(lineptr, " \t\n\r"); /* (1st token) next is in push */
 
 		if (opcode == NULL || opcode[0] == '#')
@@ -48,6 +47,7 @@ int main(int argc, char *argv[])
 		{/*find out the instruction and execute it*/
 			execute(opcode, line_number, &stack, file, lineptr);
 		}
+		line_number++;
 	} /* printf("read %d lines\n", line_number); */
 	exit_sequence(EXIT_SUCCESS, &stack, file, lineptr);
 	return (0);
